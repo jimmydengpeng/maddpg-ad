5ea3dd0659f911c51a74d1ad59381fa77b277990
jimmy <jimmy@JimmysWindows>
2022-01-06 17:29:16 +0800

    minor style fix

--- experiments/train.py
+++ experiments/train.py
@@ -1,4 +1,5 @@
 DEBUG = 1
+
 import argparse
 import numpy as np
 import tensorflow as tf
@@ -12,36 +13,39 @@ import tensorflow.contrib.layers as layers
 def parse_args():
     parser = argparse.ArgumentParser("Reinforcement Learning experiments for multiagent environments")
     # Environment
-    parser.add_argument("--scenario", type=str, default="simple", help="name of the scenario script")
+    parser.add_argument("--scenario",        type=str, default="simple", help="name of the scenario script")
     parser.add_argument("--max-episode-len", type=int, default=25, help="maximum episode length")
-    parser.add_argument("--num-episodes", type=int, default=60000, help="number of episodes")
+    parser.add_argument("--num-episodes",    type=int, default=60000, help="number of episodes")
     parser.add_argument("--num-adversaries", type=int, default=0, help="number of adversaries")
-    parser.add_argument("--good-policy", type=str, default="maddpg", help="policy for good agents")
-    parser.add_argument("--adv-policy", type=str, default="maddpg", help="policy of adversaries")
+    parser.add_argument("--good-policy",     type=str, default="maddpg", help="policy for good agents")
+    parser.add_argument("--adv-policy",      type=str, default="maddpg", help="policy of adversaries")
     # Core training parameters
-    parser.add_argument("--lr", type=float, default=1e-2, help="learning rate for Adam optimizer")
-    parser.add_argument("--gamma", type=float, default=0.95, help="discount factor")
-    parser.add_argument("--batch-size", type=int, default=1024, help="number of episodes to optimize at the same time")
-    parser.add_argument("--num-units", type=int, default=64, help="number of units in the mlp")
+    parser.add_argument("--lr",              type=float, default=1e-2, help="learning rate for Adam optimizer")
+    parser.add_argument("--gamma",           type=float, default=0.95, help="discount factor")
+    parser.add_argument("--batch-size",      type=int, default=1024, help="number of episodes to optimize at the same time")
+    parser.add_argument("--num-units",       type=int, default=64, help="number of units in the mlp")
     # Checkpointing
-    parser.add_argument("--exp-name", type=str, default=None, help="name of the experiment")
-    parser.add_argument("--save-dir", type=str, default="/tmp/policy/", help="directory in which training state and model should be saved")
-    parser.add_argument("--save-rate", type=int, default=1000, help="save model once every time this many episodes are completed")
-    parser.add_argument("--load-dir", type=str, default="", help="directory in which training state and model are loaded")
+    parser.add_argument("--exp-name",        type=str, default=None, help="name of the experiment")
+    parser.add_argument("--save-dir",        type=str, default="/tmp/policy/", help="directory in which training state and model should be saved")
+    parser.add_argument("--save-rate",       type=int, default=1000, help="save model once every time this many episodes are completed")
+    parser.add_argument("--load-dir",        type=str, default="", help="directory in which training state and model are loaded")
     # Evaluation
-    parser.add_argument("--restore", action="store_true", default=False)
-    parser.add_argument("--display", action="store_true", default=False)
-    parser.add_argument("--benchmark", action="store_true", default=False)
+    parser.add_argument("--restore",         action="store_true", default=False) ## once has this option its value will be stored as true
+    parser.add_argument("--display",         action="store_true", default=False) ## once has this option its value will be stored as true
+    parser.add_argument("--benchmark",       action="store_true", default=False) ## once has this option its value will be stored as true
     parser.add_argument("--benchmark-iters", type=int, default=100000, help="number of iterations run for benchmarking")
-    parser.add_argument("--benchmark-dir", type=str, default="./benchmark_files/", help="directory where benchmark data is saved")
-    parser.add_argument("--plots-dir", type=str, default="./learning_curves/", help="directory where plot data is saved")
+    parser.add_argument("--benchmark-dir",   type=str, default="./benchmark_files/", help="directory where benchmark data is saved")
+    parser.add_argument("--plots-dir",       type=str, default="./learning_curves/", help="directory where plot data is saved")
+
     args = parser.parse_args()
+
+    ## for debug 
     if DEBUG == 1:
         print(f"[DEBUG] args type: {type(args)}")
         print(f"[DEBUG] args: {args}")
         print(f"[DEBUG] args.exp_name: {args.exp_name}")
-        # args.scenario
-        # exit()
+        #// args.scenario
+        #// exit()
     return args
 
 def mlp_model(input, num_outputs, scope, reuse=False, num_units=64, rnn_cell=None):
@@ -197,5 +201,5 @@ def train(arglist):
                 break
 
 if __name__ == '__main__':
-    arglist = parse_args()
-    train(arglist)
+    arglist = arse_args()
+    train(arglst)
